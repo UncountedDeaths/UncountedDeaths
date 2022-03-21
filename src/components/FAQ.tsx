f28030cba996df5f163d6e7914837aeefbe384b7
toucham <thachathumam@gmail.com>
2022-03-21 17:44:03 -0400

    changed according to review

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Collapse, Typography } from 'antd';
 import '../styles/FAQ.less';
 import { FAQ_data } from '../content/FAQContent';
+import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
 
 const FAQ: React.FC = () => {
   return (
     <div className="faq-container">
-      <Collapse>
+      <Collapse
+        expandIcon={({ isActive }) => (isActive ? <MinusOutlined /> : <PlusOutlined />)}
+        expandIconPosition="right"
+      >
         {FAQ_data.map((f, i) => (
           <Collapse.Panel key={i} header={<Typography.Text strong>{f.title}</Typography.Text>}>
             <Typography.Text>{f.content}</Typography.Text>
