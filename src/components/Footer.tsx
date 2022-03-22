937ce48076cfafb7ed5790c6c9d0701218f4f1d9
lakshitaa <lakshitaa.sharma1@gmail.com>
2022-03-21 22:39:33 -0400

    suggested changes for responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from '../styles/MasterPage.module.less';
 import { BULogo } from '../assets/assets.index';
 
 export const MainFooter: React.FC = () => {
   return (
     <div style={{ width: '100vw%' }}>
-      <img src={BULogo} style={{ display: 'flex', alignItems: 'left', padding: '20px', width: '648px' }} />
+      <img src={BULogo} className={styles.FooterLogo} />
     </div>
   );
 };
