ff6c4a6512df5e226aadef7f27c4f01f85032683
toucham <thachathumam@gmail.com>
2022-04-01 13:47:26 -0400

    autoformat from husky

--- src/components/ViewTrackerButton.tsx
+++ src/components/ViewTrackerButton.tsx
@@ -4,16 +4,17 @@ import { useNavigate } from 'react-router-dom';
 import { InternalRoutes } from '../routes';
 
 interface Props {
-    content: string;
+  content: string;
 }
 
 const ViewTrackerButton: React.FC<Props> = (props: Props) => {
-    const { content }: Props = props 
-    const navigate = useNavigate()
-    return (
-        <button className={styles.button} onClick={() => navigate(InternalRoutes.TRACKER.path)}>{content}</button>
-    );
-  };
-  
-  export default ViewTrackerButton;
-  
+  const { content }: Props = props;
+  const navigate = useNavigate();
+  return (
+    <button className={styles.button} onClick={() => navigate(InternalRoutes.TRACKER.path)}>
+      {content}
+    </button>
+  );
+};
+
+export default ViewTrackerButton;
