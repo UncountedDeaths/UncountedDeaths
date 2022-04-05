04a8b7c9fff51c4eb3afe0344fabaa6b46a2a407
toucham <thachathumam@gmail.com>
2022-04-05 18:11:41 -0400

    centered text

--- src/pages/TrackerPage.tsx
+++ src/pages/TrackerPage.tsx
@@ -7,7 +7,9 @@ import ContentLayout from '../components/ContentLayout';
 export const TrackerPage: React.FC = () => {
   return (
     <ContentLayout title="EXCESS DEATH TRACKER">
-      <Title level={2}>Interactive Map</Title>
+      <div style={{ textAlign: 'center' }}>
+        <Title level={2}>Interactive Map</Title>
+      </div>
       <TableauView />
     </ContentLayout>
   );
