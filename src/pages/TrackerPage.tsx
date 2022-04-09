cd694bc1db05c1bd465267ebfee33bec47f379d1
toucham <thachathumam@gmail.com>
2022-04-09 18:25:05 -0400

    update tracker page to v3.2

--- src/pages/TrackerPage.tsx
+++ src/pages/TrackerPage.tsx
@@ -1,39 +1,32 @@
 import React from 'react';
-import Title from 'antd/lib/typography/Title';
 import TableauView from '../components/TableauView';
 import * as Content from '../content/TrackerPageContent';
 import ContentLayout from '../components/ContentLayout';
-import Paragraph from 'antd/lib/typography/Paragraph';
 
 import styles from '../styles/TrackerPage.module.less';
+import { Typography } from 'antd';
+import CustomTitle from '../components/CustomTitle';
 
 export const TrackerPage: React.FC = () => {
   return (
-    <ContentLayout title="EXCESS DEATH TRACKER">
-      <div style={{ textAlign: 'center' }}>
-        <Title level={2}>Interactive Map</Title>
+    <ContentLayout title="EXCESS DEATH TRACKER" text={Content.text_content} marginBottom={'3rem'}>
+      <div style={{ fontSize: '1.2rem', marginBottom: '4rem' }}>
+        <Typography.Text strong>{Content.description_tableau}</Typography.Text>
       </div>
       <TableauView />
-      <div className={styles.faqpagetext}>
-        <div>
-          <Title level={3} underline>
-            {Content.about_title}
-          </Title>
-          <Title level={4}>{Content.about_1_title}</Title>
-          <Paragraph>{Content.about_1_body}</Paragraph>
-          <Title level={4}>{Content.about_2_title}</Title>
-          <Paragraph>{Content.about_2_body}</Paragraph>
+      <div className={styles.trackpagetext}>
+        <div className={styles.howtotext}>
+          <CustomTitle title={Content.how_to_title} />
+          <Typography.Paragraph>{Content.how_to_content}</Typography.Paragraph>
         </div>
-        <div>
-          <Title level={3} underline>
-            {Content.key_terms_title}
-          </Title>
-          <Title level={4}>{Content.term_1_title}</Title>
-          <Paragraph>{Content.term_2_body}</Paragraph>
-          <Title level={4}>{Content.term_2_title}</Title>
-          <Paragraph>{Content.term_2_body}</Paragraph>
-          <Title level={4}>{Content.term_3_title}</Title>
-          <Paragraph>{Content.term_3_body}</Paragraph>
+        <div className={styles.trackkeyterms}>
+          <p>{Content.key_terms_title}</p>
+          <Typography.Title level={4}>{Content.term_1_title}</Typography.Title>
+          <Typography.Paragraph>{Content.term_2_body}</Typography.Paragraph>
+          <Typography.Title level={4}>{Content.term_2_title}</Typography.Title>
+          <Typography.Paragraph>{Content.term_2_body}</Typography.Paragraph>
+          <Typography.Title level={4}>{Content.term_3_title}</Typography.Title>
+          <Typography.Paragraph>{Content.term_3_body}</Typography.Paragraph>
         </div>
       </div>
     </ContentLayout>
