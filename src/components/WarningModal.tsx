c19bc94d91ce9d7630f203ff3616924a1cce3132
Ingi-Hong <ingihong@bu.edu>
2022-04-13 15:43:44 -0400

    Mobile modal

---
+++ src/components/WarningModal.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Modal } from 'antd';
+
+interface Props {
+  isMobile: boolean;
+}
+
+export const WarningModal: React.FC<Props> = (props: Props) => {
+  const { isMobile } = props;
+  const [isVisible, setVisible] = useState(true);
+  return (
+    <>
+      {isMobile ? (
+        <Modal
+          visible={isVisible}
+          title="Mobile Warning"
+          closable={true}
+          centered={true}
+          onCancel={() => setVisible(false)}
+          footer={null}
+        >
+          <p>
+            For the best experience we recommend viewing the excess death tracker on your desktop
+            device. We currently working to improve the mobile site.
+          </p>
+        </Modal>
+      ) : null}
+    </>
+  );
+};
