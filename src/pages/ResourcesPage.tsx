763c34417b56c862e53651942191d4dd3bd23ce6
Ian <30986157+IanSaucy@users.noreply.github.com>
2022-05-02 17:20:49 -0400

    Update faq (#55)
    * Added FAQ content
    
    Need to re-work content file to not include styling with content.
    
    * Updated ResourcePage style

--- src/pages/ResourcesPage.tsx
+++ src/pages/ResourcesPage.tsx
@@ -1,18 +1,67 @@
+import { Typography } from 'antd';
+import Tabs, { TabPaneProps } from 'antd/lib/tabs';
 import React from 'react';
 import ContentLayout from '../components/ContentLayout';
-import CustomTitle from '../components/CustomTitle';
 import FAQ from '../components/FAQ';
-import { FAQ_data, text_content } from '../content/FAQContent';
+import {
+  FAQ_Death_Cert_Data,
+  FAQ_FEMA_FAQ_Data,
+  FAQ_FEMA_SubHeader_Text,
+  FAQ_Medicolegal_Data,
+  text_content,
+} from '../content/FAQContent';
+import styles from '../styles/ResourcesPage.module.less';
+
+type Key = {
+  key: string;
+};
+
+const tabs: (TabPaneProps & Key)[] = [
+  {
+    key: '1',
+    tab: <p className={styles.titletabpane}>Death Certificates</p>,
+    children: (
+      <>
+        <FAQ data={FAQ_Death_Cert_Data} />
+      </>
+    ),
+  },
+  {
+    key: '2',
+    tab: <p className={styles.titletabpane}>Death Investigation System</p>,
+    children: (
+      <>
+        <FAQ data={FAQ_Medicolegal_Data} />
+      </>
+    ),
+  },
+  {
+    key: '3',
+    tab: <p className={styles.titletabpane}>FEMA: Funeral Assistance</p>,
+    children: (
+      <>
+        <div style={{ padding: '0.5rem' }} />
+        <Typography.Text style={{ alignContent: 'center' }}>
+          {FAQ_FEMA_SubHeader_Text}
+        </Typography.Text>
+        <div style={{ padding: '2rem' }} />
+        <FAQ data={FAQ_FEMA_FAQ_Data} />
+      </>
+    ),
+  },
+];
 
 const ResourcesPage: React.FC = () => {
   return (
-    <ContentLayout title="RESOURCES" text={text_content}>
+    <ContentLayout title="FREQUENTLY ASKED QUESTIONS" text={text_content}>
       <div>
-        <CustomTitle title="FEMA" />
-      </div>
-      <div>
-        <CustomTitle title="FREQUENTLY ASKED QUESTIONS" />
-        <FAQ data={FAQ_data} />
+        {/* <CustomTitle title="FREQUENTLY ASKED QUESTIONS" /> */}
+        {/* <FAQ data={FAQ_data} /> */}
+        <Tabs className={styles.resourcestabs} defaultActiveKey="1">
+          {tabs.map((t) => (
+            <Tabs.TabPane {...t} key={t.key} />
+          ))}
+        </Tabs>
       </div>
     </ContentLayout>
   );
