daa9a924670f869c78d933bfd8ca3e180b3ed112
Ian <30986157+IanSaucy@users.noreply.github.com>
2022-05-03 15:08:19 -0400

    Updated logo and resource bar color (#57)
    * Updated logo and resource bar color
    
    * Removed used variable

--- src/pages/MasterPage.tsx
+++ src/pages/MasterPage.tsx
@@ -4,7 +4,7 @@ import { Header } from '../components/NavHeader';
 import styles from '../styles/MasterPage.module.less';
 import { TrackerPage } from '../pages/TrackerPage';
 import { LandingPage } from './LandingPage';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { Page404 } from './404Page';
 import { PublicationsPage } from './PublicationsPage';
 import OurTeamPage from './OurTeamPage';
@@ -19,13 +19,20 @@ import { BULogo } from '../assets/assets.index';
  * are rendered here. Plus, the react router lives here!
  */
 export const Main: React.FC = () => {
+  const location = useLocation();
   return (
     <Layout className={styles.MasterLayout}>
       <Layout.Header className={styles.MenuHeader}>
         <PersistentBanner />
         <Header />
       </Layout.Header>
-      <Layout.Content className={styles.MasterContent}>
+      <Layout.Content
+        className={
+          location.pathname == InternalRoutes.HOME.path
+            ? styles.MasterContent
+            : styles.MasterContentNotHome
+        }
+      >
         {/* We put a router directly here so that it only has to re-load the contents
         of the page, not the entire thing! 
          */}
