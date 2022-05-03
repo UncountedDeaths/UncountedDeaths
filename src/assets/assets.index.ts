fd4f9226300df98d28800746dfd3998e2017f9e9
Ian Saucy <iansaucy@gmail.com>
2022-05-03 15:21:52 -0400

    Code and doc cleanup

--- src/assets/assets.index.ts
+++ src/assets/assets.index.ts
@@ -3,7 +3,6 @@
  */
 
 export { default as landingPage1 } from './landing_page/landing_page_img_1.png';
-export { default as landingPage2 } from './landing_page/landing_page_img_2.png';
 
 /**
  * Profile Photos
@@ -18,41 +17,9 @@ export { default as syl } from './profilePhotos/Syl.jpg';
 export { default as wubin } from './profilePhotos/Wubin.jpg';
 
 /**
- * Social Icons
- */
-
-export { ReactComponent as DevPost } from './social/devpost.svg';
-export { ReactComponent as Github } from './social/github.svg';
-export { ReactComponent as CircleEmail } from './social/circle_email.svg';
-
-/**
  * Logos and other branding
  */
 
 export { ReactComponent as Logo } from './BU_CEID_Color.svg';
 export { ReactComponent as BULogo } from './bu_wordmark.svg';
 export { ReactComponent as LogoWhite } from './BU_CEID_White.svg';
-
-/**
- * Component assets
- * Todo: Maybe move these inline to the components?
- */
-
-export { ReactComponent as FlowGraphImg } from './components/flow-graph.svg';
-export { ReactComponent as LineGraphImg } from './components/graph-line.svg';
-export { ReactComponent as CircleClose } from './components/circle-close.svg';
-export { ReactComponent as SinglePersonImg } from './components/single_person.svg';
-export { ReactComponent as DuelPersonImg } from './components/duel_person.svg';
-export { ReactComponent as FirstAidImg } from './components/firstaid_kit.svg';
-export { ReactComponent as FistImg } from './components/fist.svg';
-export { ReactComponent as HandTogetherImg } from './components/hands_together.svg';
-export { ReactComponent as HeartMobImg } from './components/heart_mob.svg';
-export { ReactComponent as SafeHorizonImg } from './components/safe_horizon.svg';
-export { ReactComponent as ChartStatsImg } from './components/stats_chart.svg';
-export { ReactComponent as ToolBoxImg } from './components/toolbox.svg';
-
-/**
- * Random
- */
-
-export { ReactComponent as SocialDistanceImg } from './social_distancing.svg';
