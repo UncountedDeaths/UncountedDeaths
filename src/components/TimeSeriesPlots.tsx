a9c173ff59537a6f5f890d63e7ec5dba130677f2
matt-henriksen <73131815+matt-henriksen@users.noreply.github.com>
2022-06-29 21:42:49 -0400

    Updated container format to match other components

--- src/components/TimeSeriesPlots.tsx
+++ src/components/TimeSeriesPlots.tsx
@@ -3,76 +3,78 @@ import { ResponsiveLine } from '@nivo/line';
 import { data } from '../content/TimeSeriesData';
 import styles from '../styles/TimeSeriesPlots.module.less';
 
-const NivoTSPlot = () => (
-  <div className={styles.TimeSeriesViewBox}>
-    <ResponsiveLine
-      data={data}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{ type: 'point' }}
-      yScale={{
-        type: 'linear',
-        min: 'auto',
-        max: 'auto',
-        stacked: true,
-        reverse: false,
-      }}
-      yFormat=" >-.2f"
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: 'transportation',
-        legendOffset: 36,
-        legendPosition: 'middle',
-      }}
-      axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: 'count',
-        legendOffset: -40,
-        legendPosition: 'middle',
-      }}
-      enableGridX={false}
-      colors={{ scheme: 'set1' }}
-      pointSize={1}
-      pointColor={{ from: 'color', modifiers: [] }}
-      pointBorderWidth={1}
-      pointBorderColor={{ from: 'serieColor' }}
-      pointLabelYOffset={-12}
-      enableCrosshair={false}
-      useMesh={true}
-      legends={[
-        {
-          anchor: 'right',
-          direction: 'column',
-          justify: false,
-          translateX: 100,
-          translateY: 0,
-          itemsSpacing: 0,
-          itemDirection: 'left-to-right',
-          itemWidth: 80,
-          itemHeight: 20,
-          itemOpacity: 0.75,
-          symbolSize: 12,
-          symbolShape: 'circle',
-          symbolBorderColor: 'rgba(0, 0, 0, .5)',
-          effects: [
-            {
-              on: 'hover',
-              style: {
-                itemBackground: 'rgba(0, 0, 0, .03)',
-                itemOpacity: 1,
+const NivoTSPlot: React.FC = () => {
+  return (
+    <div className={styles.TimeSeriesViewBox}>
+      <ResponsiveLine
+        data={data}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: 'point' }}
+        yScale={{
+          type: 'linear',
+          min: 'auto',
+          max: 'auto',
+          stacked: true,
+          reverse: false,
+        }}
+        yFormat=" >-.2f"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'transportation',
+          legendOffset: 36,
+          legendPosition: 'middle',
+        }}
+        axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'count',
+          legendOffset: -40,
+          legendPosition: 'middle',
+        }}
+        enableGridX={false}
+        colors={{ scheme: 'set1' }}
+        pointSize={1}
+        pointColor={{ from: 'color', modifiers: [] }}
+        pointBorderWidth={1}
+        pointBorderColor={{ from: 'serieColor' }}
+        pointLabelYOffset={-12}
+        enableCrosshair={false}
+        useMesh={true}
+        legends={[
+          {
+            anchor: 'right',
+            direction: 'column',
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: 'left-to-right',
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: 'circle',
+            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemBackground: 'rgba(0, 0, 0, .03)',
+                  itemOpacity: 1,
+                },
               },
-            },
-          ],
-        },
-      ]}
-      animate={false}
-    />
-  </div>
-);
+            ],
+          },
+        ]}
+        animate={false}
+      />
+    </div>
+  );
+};
 
 export default NivoTSPlot;
