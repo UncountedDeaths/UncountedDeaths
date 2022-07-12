494166e233a937f366bf62da287d85c25b9fe5cd
matt-henriksen <73131815+matt-henriksen@users.noreply.github.com>
2022-07-12 11:57:42 -0400

    Add code for the time series plot

---
+++ src/components/NivoTimeSeriesPlots.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { ResponsiveLine } from '@nivo/line';
+import { Autaga } from '../content/AutagaTimeSeriesData';
+import styles from '../styles/TimeSeriesPlots.module.less';
+
+const NivoTSPlot: React.FC = () => {
+  return (
+    <div className={styles.TimeSeriesViewBox}>
+      <ResponsiveLine
+        data={Autaga}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: 'point' }}
+        yScale={{
+          type: 'linear',
+          min: 'auto',
+          max: 'auto',
+          stacked: true,
+          reverse: false,
+        }}
+        yFormat=" >-.2f"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'date',
+          legendOffset: 36,
+          legendPosition: 'middle',
+        }}
+        axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'deaths',
+          legendOffset: -40,
+          legendPosition: 'middle',
+        }}
+        enableGridX={false}
+        colors={{ scheme: 'set1' }}
+        pointSize={1}
+        pointColor={{ from: 'color', modifiers: [] }}
+        pointBorderWidth={1}
+        pointBorderColor={{ from: 'serieColor' }}
+        pointLabelYOffset={-12}
+        enableCrosshair={false}
+        useMesh={true}
+        legends={[
+          {
+            anchor: 'right',
+            direction: 'column',
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: 'left-to-right',
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: 'circle',
+            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemBackground: 'rgba(0, 0, 0, .03)',
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+        animate={true}
+      />
+    </div>
+  );
+};
+
+export default NivoTSPlot;
