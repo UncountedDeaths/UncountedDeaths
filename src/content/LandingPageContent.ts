d60b7e03b998711403da5b86a9a70a5732bcde70
matt-henriksen <73131815+matt-henriksen@users.noreply.github.com>
2022-07-19 12:53:09 -0400

    Update "Who we are" on homepage
    Update the "Who we are" content to the following on both the home page.  Without quotes
    
    "This project is made possible through a collaboration between researchers at the Boston University School of Public Health and the University of Pennsylvania with support from the Boston University Center for Emerging Infectious Disease Policy & Research (CEID), BU Spark!, the Robert Wood Johnson Foundation, the W. K. Kellogg Foundation, and the National Institute on Aging. The views expressed on this website are those of the lab's investigators and do not necessarily reflect the views of the research sponsors."

--- src/content/LandingPageContent.ts
+++ src/content/LandingPageContent.ts
@@ -6,7 +6,7 @@ export const view_more_publications = 'View more publications';
 
 export const who_we_are_header = 'Who we are';
 export const who_we_are_body =
-  'This project is made possible through a collaboration between researchers at the Boston University School of Public Health and the University of Pennsylvania with support from the Boston University Center for Emerging Infectious Disease Policy & Research (CEID), BU Spark!, the Robert Wood Johnson Foundation, and the W. K. Kellogg Foundation.';
+  "This project is made possible through a collaboration between researchers at the Boston University School of Public Health and the University of Pennsylvania with support from the Boston University Center for Emerging Infectious Disease Policy & Research (CEID), BU Spark!, the Robert Wood Johnson Foundation, the W. K. Kellogg Foundation, and the National Institute on Aging. The views expressed on this website are those of the lab's investigators and do not necessarily reflect the views of the research sponsors.";
 export const title_text = 'Pandemic Mortality Tracker';
 export const title_body_text =
   'More than 170,000 deaths have gone uncounted across the US during the Covid-19 pandemic. Some of these deaths are deaths indirectly related to the social, economic and health system consequences of the pandemic, but most are uncounted Covid-19 deaths. Excess mortality is a tool to measure the true mortality impact of the pandemic by comparing pandemic mortality to pre-pandemic mortality.';
