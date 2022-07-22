d6bebc02eac1a5a9dba9610e80e58ab3e5848145
Henry Bayly <baylyh@bc.edu>
2022-07-22 11:31:31 -0400

    fix-formattiing

--- src/content/PublicationsPageContent.ts
+++ src/content/PublicationsPageContent.ts
@@ -17,7 +17,8 @@ export const content: ContentCardProps[] = [
     date: 'July, 2022',
     text: 'COVID-19 has disproportionately affected racial and ethnic minority groups in the US. However, estimating the full effects of the pandemic on health disparities should account for other causes of death, including external causes.2 We estimated racial and ethnic disparities in excess deaths from external causes (homicide, suicide, transportation, and drug overdoses) from March through December 2020.',
     link: 'https://jamanetwork.com/journals/jamainternalmedicine/fullarticle/2791682',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/e08e5fca-9de5-4033-6252-cd349b88d900/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/e08e5fca-9de5-4033-6252-cd349b88d900/public',
   },
   {
     title:
@@ -27,7 +28,8 @@ export const content: ContentCardProps[] = [
     date: 'July, 2022',
     text: 'Excess mortality has exceeded reported deaths from Covid-19 during the pandemic. This gap may be attributable to deaths that occurred among individuals with undiagnosed Covid-19 infections or indirect consequences of the pandemic response such as interruptions in medical care; distinguishing these possibilities has implications for public health responses.',
     link: 'https://doi.org/10.1093/pnasnexus/pgac079',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/8ee68b7a-1ed5-4d6c-7176-bc3fdf60bd00/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/8ee68b7a-1ed5-4d6c-7176-bc3fdf60bd00/public',
   },
   {
     title: 'Missing Americans: Early Death in the United States, 1933-2021',
@@ -36,7 +38,8 @@ export const content: ContentCardProps[] = [
     date: 'June, 2022',
     text: 'We assessed how many U.S. deaths would have been averted each year, 1933-2021, if U.S. age-specific mortality rates had equaled those of other wealthy nations. The annual number of excess deaths in the U.S. increased steadily beginning in the late 1970s, reaching 626,353 in 2019.',
     link: 'https://www.medrxiv.org/content/10.1101/2022.06.29.22277065v1',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/75cdd6c9-3a35-4dd6-c4df-fa9b1e66bc00/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/75cdd6c9-3a35-4dd6-c4df-fa9b1e66bc00/public',
   },
   {
     title:
@@ -45,7 +48,8 @@ export const content: ContentCardProps[] = [
     date: 'June, 2022',
     text: 'Recent research underscores the exceptionally young age distribution of Covid-19 deaths in the United States compared with international peers. This brief characterizes how high levels of Covid mortality at midlife ages (45–64) are deeply intertwined with continuing racial inequity in Covid-19 mortality.',
     link: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8923115/',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/6747f01d-d5ba-4177-24a1-c9538b33a000/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/6747f01d-d5ba-4177-24a1-c9538b33a000/public',
   },
   {
     title:
@@ -54,7 +58,8 @@ export const content: ContentCardProps[] = [
     date: 'June, 2022',
     text: "Despite a growing body of literature focused on racial/ethnic disparities in Covid-19 mortality, few previous studies have examined the pandemic's impact on 2020 cause-specific mortality by race and ethnicity. This paper documents changes in mortality by underlying cause of death and race/ethnicity between 2019 and 2020.",
     link: 'https://doi.org/10.1016/j.ssmph.2021.101012',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/eddb11d1-e140-4437-7874-504616a6dc00/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/eddb11d1-e140-4437-7874-504616a6dc00/public',
   },
   {
     title:
@@ -64,7 +69,8 @@ export const content: ContentCardProps[] = [
     date: 'May, 2022',
     text: 'Official Covid-19 death tallies have underestimated the mortality impact of the Covid-19 pandemic in the United States. Excess mortality, which compares observed deaths to deaths expected in the absence of the pandemic, is a useful measure for assessing the total effect of the pandemic on mortality levels.',
     link: 'https://www.medrxiv.org/content/10.1101/2022.04.23.22274192v2',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/1b6438a4-0ba4-45e0-71b2-b3506b87b600/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/1b6438a4-0ba4-45e0-71b2-b3506b87b600/public',
   },
   {
     title:
@@ -73,7 +79,8 @@ export const content: ContentCardProps[] = [
     date: 'May, 2022',
     text: 'The disproportionate burden of COVID-19 mortality among older adults during the early stages of the pandemic is well documented.However, little is known about associations between subsequent developments (eg, vaccine availability) and the age pattern of mortality. In this study, we examined changes in age-specific COVID-19 mortality rates by sex and by race and ethnicity.',
     link: 'https://jamanetwork.com/journals/jamanetworkopen/fullarticle/2792375',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/22a937db-55bf-42c7-7324-fe30de848900/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/22a937db-55bf-42c7-7324-fe30de848900/public',
   },
   {
     title:
@@ -83,7 +90,8 @@ export const content: ContentCardProps[] = [
     date: 'April, 2022',
     text: 'A small body of epidemiological research suggests that working in an essential sector is a risk factor for SARS-CoV-2 infection or subsequent disease or mortality. However, there is limited evidence to date on the US, or on how the risks associated with essential work differ across demographic subgroups defined by age, sex, and race/ethnicity.',
     link: 'https://www.medrxiv.org/content/10.1101/2022.03.29.22273085v2',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/6d6b5e96-2f85-491a-6b8d-3094f53aa200/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/6d6b5e96-2f85-491a-6b8d-3094f53aa200/public',
   },
   {
     title:
@@ -93,7 +101,8 @@ export const content: ContentCardProps[] = [
     date: 'February, 2022',
     text: 'During the first year of the pandemic, essential workers faced higher rates of SARS-CoV-2 infection and COVID-19 mortality than non-essential workers. It is unknown whether disparities in pandemic-related mortality across occupational sectors have continued to occur, amidst SARS-CoV-2 variants and vaccine availability.',
     link: 'https://www.medrxiv.org/content/10.1101/2022.02.14.22270958v1',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/083c442b-6b15-463a-4aab-8d9f07ebc300/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/083c442b-6b15-463a-4aab-8d9f07ebc300/public',
   },
   {
     title:
@@ -102,7 +111,8 @@ export const content: ContentCardProps[] = [
     date: 'January, 2022',
     text: 'The COVID-19 pandemic in the U.S. has been largely monitored using death certificates containing reference to COVID-19. However, prior analyses reveal that a significant percentage of excess deaths associated with the pandemic were not directly assigned to COVID-19.',
     link: 'https://doi.org/10.1016/j.ssmph.2021.101021',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/474e5b65-b180-4528-f6bf-5d275f42ce00/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/474e5b65-b180-4528-f6bf-5d275f42ce00/public',
   },
   {
     title:
@@ -111,7 +121,8 @@ export const content: ContentCardProps[] = [
     date: 'September, 2021',
     text: 'Approximately 20% of excess deaths in the US in 2020 were not reflected in COVID-19 death counts. These excess deaths included deaths caused by COVID-19 but not assigned to it as well as indirect deaths from other causes associated with delays in health care and the social and economic consequences of the pandemic.',
     link: 'https://jamanetwork.com/journals/jamanetworkopen/fullarticle/2783986',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/a15e7546-f534-4b8c-d4e9-c776b0780500/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/a15e7546-f534-4b8c-d4e9-c776b0780500/public',
   },
   {
     title:
@@ -120,7 +131,8 @@ export const content: ContentCardProps[] = [
     date: 'April, 2021',
     text: 'Quality death investigation is a critical piece of an effective public health system. When a person dies, a coroner, medical examiner, or health professional with knowledge of the decedent’s medical conditions fills out the death certificate. Accurate cause-of-death ascertainment has broad implications for understanding the burden of disease throughout the United States.',
     link: 'https://ajph.aphapublications.org/doi/full/10.2105/AJPH.2021.306331',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/8e08b40d-b92b-495f-c81c-9f6f7146d000/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/8e08b40d-b92b-495f-c81c-9f6f7146d000/public',
   },
   {
     title: 'COVID-19 and excess mortality in the United States: A county-level analysis',
@@ -128,7 +140,8 @@ export const content: ContentCardProps[] = [
     date: 'May, 2021',
     text: 'Coronavirus Disease 2019 (COVID-19) excess deaths refer to increases in mortality over what would normally have been expected in the absence of the COVID-19 pandemic. Several prior studies have calculated excess deaths in the United States but were limited to the national or state level, precluding an examination of area-level variation in excess mortality and excess deaths not assigned to COVID-19.',
     link: 'https://doi.org/10.1371/journal.pmed.1003571',
-    imgSrc: 'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/c7112b68-5102-45b4-90ad-f48d83556a00/public',
+    imgSrc:
+      'https://imagedelivery.net/TAwaCWi-w4YvJ7p6xuj9Jg/c7112b68-5102-45b4-90ad-f48d83556a00/public',
   },
 ];
 
