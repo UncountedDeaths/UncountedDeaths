0ea3f461fd5ccd8006ff110f74f490d45235e3c5
matt-henriksen <73131815+matt-henriksen@users.noreply.github.com>
2022-08-02 17:23:08 -0400

    Transfer in progress changes to new branch

--- src/components/countyDashboard.tsx
+++ src/components/countyDashboard.tsx
@@ -2,6 +2,7 @@ import { Select } from 'antd';
 import React, { useState } from 'react';
 import countyData from '../content/stateDashContent';
 import { stateData } from '../content/stateDashContent';
+import { dataTypes } from '../content/stateDashContent';
 const { Option } = Select;
 import styles from '../styles/dashboard.module.less';
 import IframeResizer from 'iframe-resizer-react';
@@ -9,6 +10,11 @@ import IframeResizer from 'iframe-resizer-react';
 import NivoTSPlot from '../components/NivoTimeSeriesPlots';
 
 type CountyName = keyof typeof countyData;
+type DataType =
+  | 'Excess Death Rates'
+  | 'Covid-19 Death Rates'
+  | 'Potential Undercounting'
+  | 'Medical Death Investigation System';
 
 type ControlsPropsType = {
   onStateChange: (state: CountyName) => void;
@@ -31,6 +37,7 @@ const DashboardControls: React.FC<ControlsPropsType> = (props) => {
 const CountyDashboard: React.FC = () => {
   const [currState, setCurrState] = useState(stateData[0]);
   const [currCounty, setCurrCounty] = useState(countyData[stateData[0] as CountyName][0]);
+  const [currData, setCurrData] = useState(dataTypes[0] as DataType[0]);
 
   const handleStateChange = (value: CountyName) => {
     setCurrState(value);
@@ -41,12 +48,84 @@ const CountyDashboard: React.FC = () => {
     setCurrCounty(value);
   };
 
+  const handleDataTypeChange = (value: DataType) => {
+    setCurrData(value);
+  };
+
   const stateCounties = countyData[currState as CountyName];
 
   // should change when currState is updated. Shows map of counties in a state
   const stateCountiesDashboardURL = 'https://datawrapper.dwcdn.net/q9LZ6/3/';
   // should change when currCounty is updated. Shows map of a county
   const countiesDashboardURL = 'https://datawrapper.dwcdn.net/EviBb/2/';
+
+  const ExcessDeathRates = () => {
+    return (
+      <div className={styles.stateDashboard}>
+        <IframeResizer
+          className={[styles.appIFrame, styles.appIFrameSmall].join(' ')}
+          src="https://datawrapper.dwcdn.net/BJ7PN/"
+        />
+        <IframeResizer
+          className={[styles.appIFrame, styles.appIFrameSmall].join(' ')}
+          src="https://datawrapper.dwcdn.net/UrD9D/"
+        />
+      </div>
+    );
+  };
+
+  const CovidDeathRates = () => {
+    return (
+      <div className={styles.stateDashboard}>
+        <IframeResizer
+          className={[styles.appIFrame, styles.appIFrameSmall].join(' ')}
+          src="https://datawrapper.dwcdn.net/E5zKi/"
+        />
+        <IframeResizer
+          className={[styles.appIFrame, styles.appIFrameSmall].join(' ')}
+          src="https://datawrapper.dwcdn.net/jYuD6/"
+        />
+      </div>
+    );
+  };
+
+  const PotentialUndercounting = () => {
+    return (
+      <div className={styles.stateDashboard}>
+        <IframeResizer
+          className={[styles.appIFrame, styles.appIFrameSmall].join(' ')}
+          src="https://datawrapper.dwcdn.net/Otwmm/"
+        />
+        <IframeResizer
+          className={[styles.appIFrame, styles.appIFrameSmall].join(' ')}
+          src="https://datawrapper.dwcdn.net/HXhjy/"
+        />
+      </div>
+    );
+  };
+
+  const MedicalDeathInvestigationSystem = () => {
+    return (
+      <div className={styles.stateDashboard}>
+        <IframeResizer
+          className={[styles.appIFrame, styles.appIFrameSmall].join(' ')}
+          src="https://datawrapper.dwcdn.net/OLxLq/"
+        />
+      </div>
+    );
+  };
+
+  function DisplayIframe(data: string) {
+    if (data === 'Excess Death Rates') {
+      return <ExcessDeathRates />;
+    } else if (data === 'Covid-19 Death Rates') {
+      return <CovidDeathRates />;
+    } else if (data === 'Potential Undercounting') {
+      return <PotentialUndercounting />;
+    } else if (data === 'Medical Death Investigation System') {
+      return <MedicalDeathInvestigationSystem />;
+    } else return <p>How did we get here?</p>;
+  }
 
   return (
     <div className={styles.dashboardTab}>
@@ -65,16 +144,20 @@ const CountyDashboard: React.FC = () => {
             ))}
           </Select>
         </div>
+        <div className={styles.bothDrop}>
+          <Select
+            style={{ width: 170 }}
+            value={currData as DataType}
+            onChange={handleDataTypeChange}
+          >
+            {dataTypes.map((state) => (
+              <Option key={state}>{state}</Option>
+            ))}
+          </Select>
+        </div>
       </div>
       <div className={styles.stateDashboard}>
-        <IframeResizer
-          className={[styles.appIFrame, styles.appIFrameSmall].join(' ')}
-          src={stateCountiesDashboardURL}
-        />
-        <IframeResizer
-          className={[styles.appIFrame, styles.appIFrameSmall].join(' ')}
-          src={countiesDashboardURL}
-        />
+        <DisplayIframe data={currData} />
       </div>
       <NivoTSPlot />
     </div>
