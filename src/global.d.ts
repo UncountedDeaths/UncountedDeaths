3261a4aba156a0cc7b3e799584cfb8014075a04f
Ian Saucy <iansaucy@gmail.com>
2022-08-31 20:05:58 -0400

    fixed type error

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,4 +2,5 @@ declare module '*.module.less' {
   const classes: { [key: string]: string };
   export default classes;
 }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare let datawrapper: any;
